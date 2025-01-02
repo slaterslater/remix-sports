777b9da3d4eeefcf1380b3d1a0f577624ba246e4
slaterslater <slaterslater@gmail.com>
2025-01-02 16:06:14 -0500

    wip

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,105 @@
-import type { MetaFunction } from "@vercel/remix";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+} from "@remix-run/react"
+import {
+  type ActionFunctionArgs,
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@vercel/remix"
+import * as cheerio from "cheerio"
+import { useEffect, useState } from "react"
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    { title: "Remix Sports" },
+    { name: "description", content: "Sports Headlines" },
+  ]
+}
 
-export default function Index() {
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+  const page = "1"
+  const news = await getPlayerNewsPost(page)
+
+  return json({ news })
+}
+
+export const action = async ({ request, params }: ActionFunctionArgs) => {
+  const formData = await request.formData()
+  const page = formData.get("nextpage")
+  const headlines = await getPlayerNewsPost(page as string)
+
+  return json({ headlines })
+}
+
+export const getPlayerNewsPost = async (page: string | number) => {
+  const URL = `https://www.nbcsports.com/fantasy/football/player-news?f0=Headline&p=${page}`
+  const resp = await fetch(URL)
+  const text = await resp.text()
+
+  const $ = cheerio.load(text)
+  const playerNewsPost = $(".PlayerNewsPost")
+    .map(function () {
+      const postClone = $(this).clone()
+      postClone.find(".PlayerNewsPost-footer").remove()
+      return postClone.html()
+    })
+    .toArray()
+
+  return playerNewsPost
+}
+
+const Headlines = ({ news }: { news: string[] | undefined }) => {
+  if (!news) return null
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div className="z-10 w-full max-w-lg items-center justify-between font-mono text-sm lg:flex">
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {news.map((__html, i) => (
+          <li key={i} className="mb-10 border-b pb-5 snap-y">
+            <div dangerouslySetInnerHTML={{ __html }} />
+          </li>
+        ))}
       </ul>
     </div>
-  );
+  )
+}
+
+export default function Index() {
+  const { news } = useLoaderData<typeof loader>()
+  const moreNews = useActionData<typeof action>()
+
+  const [page, setPage] = useState(1)
+  const [headlines, setHeadlines] = useState(news)
+
+  useEffect(() => {
+    if (!moreNews) return
+    setHeadlines((prev) => [...prev, ...moreNews.headlines])
+  }, [moreNews])
+
+  const nextPage = () => setPage((prev) => (prev += 1))
+
+  return (
+    <main>
+      {/* <nav>
+        <ul>
+          <li><Link href="/" title="go to top">NFL</Link></li>
+          <li><Link href="/" title="go to top">Jays</Link></li>
+          <li><Link href="/" title="go to top"><IoMdRefresh /></Link></li>
+        </ul>
+      </nav> */}
+      <Headlines news={headlines} />
+      <Form
+        method="POST"
+        onSubmit={nextPage}
+        preventScrollReset={true}
+        // key={page}
+      >
+        <input type="hidden" value={page + 1} name="nextpage" />
+        <button type="submit">MORE</button>
+      </Form>
+    </main>
+  )
 }
