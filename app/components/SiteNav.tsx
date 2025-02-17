f255d988946ec67d2b29f0ce505f28131f7e4576
slaterslater <slaterslater@gmail.com>
2025-02-17 14:19:11 -0500

    * renders defaultValue on select element instead of selected property on option elements * adds onchange on select elements instead of form element * refactors handleChange function

--- app/components/SiteNav.tsx
+++ app/components/SiteNav.tsx
@@ -3,32 +3,47 @@ import {
   useLocation,
   useNavigate,
   useRevalidator,
+  useSearchParams,
 } from "@remix-run/react"
 import { IoMdRefresh } from "react-icons/io"
 import Spinner from "./Spinner"
 import { useEffect } from "react"
 
-const PREFERENCE = "remix_sports_url"
+const SPORT = "sport-preference"
+const NEWS = "news-preference"
 
 export function SiteNav() {
   const navigate = useNavigate()
+  const location = useLocation()
+  const revalidator = useRevalidator()
+  const [searchParams, setSearchParams] = useSearchParams()
 
   useEffect(() => {
-    const preference = window.localStorage.getItem(PREFERENCE)
-    const url = preference ?? "/fantasy/football?news=headlines"
+    const sport = window.localStorage.getItem(SPORT) ?? "fantasy/football"
+    const news = window.localStorage.getItem(NEWS) ?? "headlines"
+    let url = `${sport}?news=${news}`
     navigate(url)
   }, [navigate])
 
-  const handleChange = (event: React.FormEvent | undefined) => {
-    const form = event?.currentTarget as HTMLFormElement
-    const formData = new FormData(form)
-    const { sport, newsType } = Object.fromEntries(formData)
-    const url = `${sport}?news=${newsType}`
-    window.localStorage.setItem(PREFERENCE, url)
-    navigate(url)
+  const handleChange = ({ name, value }: { name: string; value: string }) => {
+    switch (name) {
+      case "sport":
+        window.localStorage.setItem(SPORT, value)
+        const news = searchParams.get("news")
+        navigate(`${value}?news=${news}`)
+        break
+      case "newsType":
+        window.localStorage.setItem(NEWS, value)
+        setSearchParams((prev) => {
+          prev.set("news", value)
+          return prev
+        })
+        break
+      default:
+        return
+    }
   }
 
-  const revalidator = useRevalidator()
   const isIdle = revalidator.state === "idle"
 
   const refresh = () => {
@@ -36,28 +51,26 @@ export function SiteNav() {
     revalidator.revalidate()
   }
 
-  const location = useLocation()
-  const { pathname, search } = location
-
-  interface CurrentLocation {
-    [key: string]: string
-  }
-  const current: CurrentLocation = {
-    sport: pathname.substring(1),
-    newsType: search.split("=")[1],
+  const current: { [key: string]: string } = {
+    sport: location.pathname.substring(1),
+    newsType: location.search.split("=")[1],
   }
 
   return (
     <nav>
-      <Form onChange={handleChange}>
-        {Object.keys(navOptions).map((name) => (
-          <select name={name} key={name}>
-            {navOptions[name].map(({ key, value }) => (
-              <option
-                key={key}
-                value={value}
-                selected={current[name] === value}
-              >
+      <Form key={JSON.stringify(current)}>
+        {Object.keys(navOptions).map((select) => (
+          <select
+            key={select}
+            name={select}
+            defaultValue={current[select]}
+            onChange={(e) => {
+              const { name, value } = e.target
+              handleChange({ name, value })
+            }}
+          >
+            {navOptions[select].map(({ key, value }) => (
+              <option key={key} value={value}>
                 {key}
               </option>
             ))}
